b4a69cf7400f0f0ee04d22b070a503e6c50fcdb1
Nickick-ICRS <nrh16@ic.ac.uk>
2018-12-25 18:42:40 +0000

    Added software PWM code

---
+++ src/sumobot_drive/include/motor_pwm.h
@@ -0,0 +1,46 @@
+#ifndef __MOTOR_PWM_H
+#define __MOTOR_PWM_H
+
+#include <wiringPi.h>
+#include <thread>
+#include <chrono>
+
+/*
+ * PWM Control for the motors used in The Shoveller
+ * When compiling ensure to include -lwiringPi for wiringPi
+ */
+
+class PWMController {
+public:
+    PWMController(int pwm_frequency_hz, 
+                  char motor_pwm_pin, 
+                  char motor_direction_pin);
+    ~PWMController();
+
+    // Set the duty cycle on period as a percentage of the total duty cycle
+    void set_motor_power(char percent);
+    // Change the direction of the motor
+    void set_motor_direction(bool direction) { m_direction = direction; };
+private:
+    // Length of the duty cycle
+    float m_duty_cycle_length_us;
+    // Duty cycle on and off period lengths in us
+    volatile float m_duty_cycle_on_us;
+    volatile float m_duty_cycle_off_us;
+    // Current state of output - on or off
+    bool m_current_state;
+    // Current direction / state of direction pin
+    volatile bool m_direction;
+    // Pins
+    char m_pwm_pin;
+    char m_direction_pin;
+    
+    // Worker thread to regularly update the PWM pin
+    std::thread *pwm_thread;
+    // Flag to signal when the thread should end
+    bool m_running;
+    // Function to update the PWM state of the motor - run by above thread
+    void update_PWM();
+}
+
+#endif // MOTOR_PWM_H
---
+++ src/sumobot_drive/src/motor_pwm.cpp
@@ -0,0 +1,59 @@
+#include "motor_pwm.h"
+
+PWMController::PWMController(int pwm_frequency_hz,
+                             char motor_pwm_pin,
+                             char motor_direction_pin):
+                             m_pwm_pin(motor_pwm_pin),
+                             m_direction_pin(motor_direction_pin),
+                             m_duty_cycle_on_us(0),
+                             m_current_state(0),
+                             m_direction(0),
+                             m_running(true) {
+    m_duty_cycle_length_us = 1000.0f / (float)pwm_frequency_hz;
+    pinMode(m_direction_pin, OUTPUT);
+    pinMode(m_pwm_pin, OUTPUT);
+    // Initialise thread to update the PWM cycle
+    pwm_thread = new std::thread(&PWMController::update_PWM, this);
+    pwm_thread.detach();
+}
+
+PWMController::~PWMController() {
+    running = false;
+    pwm_thread.join();
+    delete pwm_thread;
+}
+
+void PWMController::set_motor_power(char percent) {
+    // If the direction pin is off then the duty cycle is on for full speed
+    if(!m_direction) {
+        m_duty_cycle_on_us = percent * m_duty_cycle_length_us / 100.0f;
+    }
+    // Otherwise the duty cycle is off for full speed
+    else {
+        m_duty_cycle_on_us = (100 - percent) * m_duty_cycle_length_us / 100.0f;
+    }
+    m_duty_cycle_off_us = m_duty_cycle_length_us - m_duty_cycle_on_us;
+}
+
+void PWMController::set_motor_direction(bool direction) {
+    m_direction = direction;
+    pinMode(
+
+void PWMController::update_PWM() {
+    using namespace std::chrono;
+
+    auto current_time = high_resolution_clock::now();
+
+    while(running) {
+        auto dt = duration_cast<microseconds>(
+            high_resolution_clock::now() - current_time
+        );
+        // Update pin output depending on state and time since last change
+        if((m_current_state && dt >=  m_duty_cycle_on_us) ||
+           (!m_current_state && dt >= m_duty_cycle_off_us)) {
+            current_time += dt;
+            m_current_state = !m_current_state;
+            digitalWrite(m_pwm_pin, m_current_state);
+        }
+    }
+}
