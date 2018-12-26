d78353d217bc04204af6d688e3d9f10b1c6999d4
Nickick-ICRS <nrh16@ic.ac.uk>
2018-12-26 17:20:56 +0000

    Added listener to listen for external commands

---
+++ src/sumobot_drive/include/listener.h
@@ -0,0 +1,37 @@
+#ifndef __LISTENER_H
+#define __LISTENER_H
+
+#include <ros/ros.h>
+#include <string>
+#include <sumobot_drive/MotorPowers.h>
+
+/*
+ * A class to listen on a ROS topic for motor instructions
+ */
+
+// Masks to easily get the power and direction from the motors
+#define DIRECTION_MASK 0b10000000
+#define POWER_MASK     0b01111111
+
+class Listener {
+public:
+    Listener(ros::NodeHandle *n, std::string topic_name);
+    ~Listener();
+
+    char get_left_motor_target() { return m_left_motor_target; };
+    char get_right_motor_target() { return m_right_motor_target; };
+
+private:
+    // Target motor powers received from the topic
+    // The 1st bit is the direction
+    char m_left_motor_target;
+    char m_right_motor_target;
+    // The subscriber which listens to the topic
+    ros::Subscriber m_subscriber;
+    // Pointer to this node's handle
+    ros::NodeHandle *node_handle;
+    // Callback to process information from the topic
+    void receive_motor_instructions(const sumobot_drive::MotorPowers::ConstPtr& msg); 
+};
+
+#endif // __LISTENER_H
---
+++ src/sumobot_drive/src/listener.cpp
@@ -0,0 +1,34 @@
+#include "listener.h"
+
+Listener::Listener(ros::NodeHandle *n, std::string topic_name) {
+    node_handle = n;
+    // Subscribe to the topic and register the callback
+    m_subscriber = 
+        node_handle->subscribe(topic_name, 
+                              100, 
+                              &Listener::receive_motor_instructions, 
+                              this);
+    m_left_motor_target = 0;
+    m_right_motor_target = 0;
+}
+
+Listener::~Listener() {
+    // dtor
+}
+
+void Listener::receive_motor_instructions(const sumobot_drive::MotorPowers::ConstPtr& msg) {
+    // Set motor powers based on received values
+    if(msg->left_power > 100)
+        m_left_motor_target = 100;
+    else
+        m_left_motor_target = msg->left_power;
+    if(msg->left_direction)
+        m_left_motor_target |= DIRECTION_MASK;
+    
+    if(msg->right_power > 100)
+        m_right_motor_target = 100;
+    else
+        m_right_motor_target = msg->right_power;
+    if(msg->right_direction)
+        m_right_motor_target |= DIRECTION_MASK;
+}
