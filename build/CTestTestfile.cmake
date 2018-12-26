669c8fbaf852beb59728d98e8d2e166ebbb67abc
Nickick-ICRS <nrh16@ic.ac.uk>
2018-12-26 17:17:32 +0000

    Updated .gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nickick/sumobot_ws/src
-# Build directory: /home/nickick/sumobot_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("sumobot_drive")
